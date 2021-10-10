da88d45995096a2dd733277c3bcab7126474c6e8
plooox <insukim97@gmail.com>
2021-10-10 08:07:42 +0000

    Basic layout Complete

--- app.py
+++ app.py
@@ -1,25 +1,65 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 @st.cache
-def load_data():
-    data = pd.read_csv("son_summary.csv")
+def load_data(option):
+    data = pd.read_csv("./"+option+".csv")
     return data
 
-st.title("Trend about Son")
+option = st.selectbox('Please select in selectbox!',
+                    ('손흥민','이강인','황의조','페이커'))
 
-son = load_data()
+st.title("Trend about " + option)
 
-dataList = np.array([son['positive'].tolist(), son['negative'].tolist()]).transpose()
 
-chart_data = pd.DataFrame(
-    dataList, index=son['date'].tolist(), columns=['pos', 'neg'])
+personalData = load_data(option)
+newData = pd.DataFrame({"date":"0000-00","positive":[0],"negative":[0]})
+# newData = newData.append({"date":"1111-11","positive":1,"negative":1}, ignore_index=True)
+totalPos = 0
+totalNeg = 0
+pos = 0
+neg = 0
+prevDate = "0000-00"
 
-st.line_chart(chart_data)
+for idx, rowData in personalData.iterrows():
+    # print(rowData)
+    curDate = rowData["Date"].split(' ')[0][:-3]
 
-chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
+    if(curDate != prevDate):
+        newData = newData.append({"date":prevDate,"positive":pos,"negative":neg}, ignore_index=True)
+        totalPos += pos
+        totalNeg += neg
+        neg = 0
+        pos = 0
+        prevDate = curDate
+        
+        if(rowData["Pos/Neg"] == 1):
+            pos +=  1
+        else:
+            neg += 1
 
-st.line_chart(chart_data)
+    else:
+        if(rowData["Pos/Neg"] == 1):
+            pos +=  1
+        else:
+            neg += 1
+
+newData = newData.append({"date":prevDate,"positive":pos,"negative":neg}, ignore_index=True)
+
+newData = newData.set_index('date')
+resData = newData.drop(['0000-00'])
+
+# dataList = np.array([personalData['positive'].tolist(), personalData['negative'].tolist()]).transpose()
+# res_data = pd.DataFrame(
+#     dataList, index=personalData['date'].tolist(), columns=['pos', 'neg'])
+totalLabel = 'positive','negative'
+totalSize = [totalPos, totalNeg]
+pieFig, pieAxis = plt.subplots()
+pieAxis.pie(totalSize, labels=totalLabel, startangle=90)
+pieAxis.axis('equal')
+
+st.pyplot(pieFig)
+st.line_chart(resData)
+
--- test.py
+++ test.py
@@ -1,18 +1,55 @@
+from os import altsep, curdir
 import pandas as pd
+from pandas.core.indexes.base import Index
 import numpy as np
-import csv
+
+personalData = pd.read_csv("./손흥민.csv")
+
+newData = pd.DataFrame({"date":"0000-00","positive":[0],"negative":[0]})
+# newData = newData.append({"date":"1111-11","positive":1,"negative":1}, ignore_index=True)
+totalPos = 0
+totalNeg = 0
+pos = 0
+neg = 0
+prevDate = "0000-00"
 
 
-def load_data():
-    data = pd.read_csv("son_summary.csv")
-    return
+for idx, rowData in personalData.iterrows():
+    # print(rowData)
+    curDate = rowData["Date"].split(' ')[0][:-3]
 
+    if(curDate != prevDate):
+        newData = newData.append({"date":prevDate,"positive":pos,"negative":neg}, ignore_index=True)
+        totalPos += pos
+        totalNeg += neg
+        neg = 0
+        pos = 0
+        prevDate = curDate
+        
+        if(rowData["Pos/Neg"] == 1):
+            pos +=  1
+        else:
+            neg += 1
 
-son = pd.read_csv("son_summary.csv")
-print([son['positive'], son['negative']])
+    else:
+        if(rowData["Pos/Neg"] == 1):
+            pos +=  1
+        else:
+            neg += 1
 
-dataList = np.array(
-    [son['positive'].tolist(), son['negative'].tolist()]).transpose()
+newData = newData.append({"date":prevDate,"positive":pos,"negative":neg}, ignore_index=True)
 
-chart_data = pd.DataFrame(dataList, index = son['date'].tolist(), columns=['pos', 'neg'])
-print(chart_data)
+newData = newData.set_index('date')
+resData = newData.drop(['0000-00'])
+        
+print("===============================")
+cmpData = pd.read_csv("./son_data.csv")
+dataList = np.array([cmpData['positive'].tolist(), cmpData['negative'].tolist()]).transpose()
+res_data = pd.DataFrame(
+    dataList, index=cmpData['date'].tolist(), columns=['pos', 'neg'])
+
+print(cmpData)
+print("===============================")
+print(res_data)
+totalData = pd.DataFrame(np.array([totalPos,totalNeg]),columns=['number of data'],index=['positive','negative'])
+print(totalData)
