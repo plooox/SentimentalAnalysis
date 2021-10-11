766c57a1178e02262eb0e3138dccc6219f0642b6
plooox <insukim97@gmail.com>
2021-10-11 08:23:17 +0000

    Add keywords

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
+from pandas.core.frame import DataFrame
 import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from krwordrank.sentence import summarize_with_sentences
+import pickle
 
 @st.cache
 def load_data(option):
-    data = pd.read_csv("./"+option+".csv")
+    data = pd.read_csv("./data/"+option+".csv")
     return data
 
 option = st.selectbox('Please select in selectbox!',
                     ('손흥민','이강인','황의조','페이커'))
 
-st.title("Trend about " + option)
-
+st.title("About " + option)
 
 personalData = load_data(option)
 newData = pd.DataFrame({"date":"0000-00","positive":[0],"negative":[0]})
@@ -51,6 +53,24 @@ newData = newData.append({"date":prevDate,"positive":pos,"negative":neg}, ignore
 newData = newData.set_index('date')
 resData = newData.drop(['0000-00'])
 
+with open("./data/"+option+".pkl", 'rb') as file:
+    twContent = pickle.load(file)
+
+penalty = lambda x:0 if (25 <= len(x) <= 80) else 1
+stopwords = {option, '출처'}
+
+keywords, sents = summarize_with_sentences(
+    twContent,
+    penalty=penalty,
+    stopwords = stopwords,
+    diversity=0.5,
+    num_keywords=100,
+    num_keysents=10,
+    verbose=False
+)
+topFive = list(keywords.keys())[:5]
+topFiveKeywords = pd.DataFrame(topFive, index={'Top1','Top2','Top3','Top4','Top5'})
+
 # dataList = np.array([personalData['positive'].tolist(), personalData['negative'].tolist()]).transpose()
 # res_data = pd.DataFrame(
 #     dataList, index=personalData['date'].tolist(), columns=['pos', 'neg'])
@@ -60,6 +80,16 @@ pieFig, pieAxis = plt.subplots()
 pieAxis.pie(totalSize, labels=totalLabel, startangle=90)
 pieAxis.axis('equal')
 
-st.pyplot(pieFig)
+col1, col2 = st.columns(2)
+txtInput = ""
+
+with col1:
+    st.subheader("Total")
+    st.pyplot(pieFig)
+with col2:
+    st.subheader("Top 5 Keywords")
+    for keyword in topFive:
+        st.button(keyword)
+st.subheader("Trends about "+option)
 st.line_chart(resData)
 
